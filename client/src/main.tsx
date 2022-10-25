f5b6b44e9af13d322a49883e4d8eefc88f22e141
julio.tassinari <julio.tassinari@leanonsystems.com>
2022-10-25 14:46:28 -0300

    Added Bootstrap Material Design

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-// Bootstrap CSS
+// Bootstrap 
 import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+
+// Material Design Bootstrap
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import './assets/sass/global.scss'
 
