fdaf57655ef5cbf70f9ec88436ab8aedc667172a
julio.tassinari <julio.tassinari@leanonsystems.com>
2022-10-26 08:40:24 -0300

    Added users collection

--- admin/src/collections/index.tsx
+++ admin/src/collections/index.tsx
@@ -1,9 +1,11 @@
 import pagesCollection from "./pages"
 import imagesCollection from "./images"
 import socialCollection from "./social"
+import usersCollection from "./users"
 
 export default [
     pagesCollection,
     imagesCollection,
-    socialCollection
+    socialCollection,
+    usersCollection
 ]
