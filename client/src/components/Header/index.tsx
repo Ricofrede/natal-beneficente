40e2a355bb0adb626760531edebe877370db9e6d
julio.tassinari <julio.tassinari@leanonsystems.com>
2022-10-28 22:41:53 -0300

    Adding new colors and styles

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,11 +1,37 @@
 import { useQuery } from 'react-query'
 import { Link } from 'react-router-dom'
 
-import { getPages, Page } from '../../firebase/functions'
+import { getPages, Page, Social, getSocials } from '../../firebase/functions'
 import './styles.scss'
+import logo from '../../assets/imgs/android-chrome-192x192.png'
 
 export default function Header() {
 	const { data, isLoading, error } = useQuery<Page[], Error>('pages', () => getPages())
+	const {
+		data: socials, isLoading: socialsLoad, error: socialError
+	} = useQuery<Social[], Error>(`socials`, () => getSocials())
+
+
+	function renderSocials() {
+		if (isLoading) return [<></>]
+		if (error) return [<></>]
+		if (!socials || !socials.length) return [<></>]
+
+		return socials?.map((social, index) => {
+			return (
+				<li className="nav-item" key={`social-header-${index}`}>
+					<a
+						className="btn btn-link btn-floating btn-lg text-dark m-3"
+						href={social.url}
+						target="_blank"
+						rel="noreferrer"
+						role="button"
+						data-mdb-ripple-color="dark"
+					><i className={`fab fa-2x ${social.iconClass}`}></i></a>
+				</li>
+			)
+		})
+	}
 
 	function renderMenu() {
 
@@ -13,15 +39,14 @@ export default function Header() {
 			const links = [1, 2, 3]
 			return links.map(link => {
 				return (
-					<p key={link} className="placeholder-glow" style={{ borderRadius: '15%', minWidth: '70px', margin: '0 10px' }}>
+					<p key={`load-header-${link}`} className="placeholder-glow" style={{ borderRadius: '15%', minWidth: '70px', margin: '0 10px' }}>
 						<span className="placeholder placeholder-lg col-12"></span>
 					</p>
 				)
 			})
 		}
 
-		if (error) return [<div>{error.message}</div>]
-
+		if (error) return [<></>]
 		if (!data || !data.length) return [<></>]
 
 		return data.map(page => {
@@ -38,14 +63,19 @@ export default function Header() {
 	}
 
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<div className="container-fluid">
-				<Link className="navbar-brand" to="/">Projeto Natal Feliz</Link>
+		<nav className="navbar navbar-expand-lg">
+			<div className="container container-fluid">
+				<Link className="navbar-brand" to="/">
+					<img src={logo} alt="Projeto Natal Feliz" />
+				</Link>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 					<i className="fas fa-bars"></i>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarNav">
 					{renderMenu()}
+					<ul className="navbar-nav nav-socials">
+						{renderSocials()}
+					</ul>
 				</div>
 			</div>
 		</nav>
