1cd9513db10904e2913511403e4ae6f1b82eec8c
julio.tassinari <julio.tassinari@leanonsystems.com>
2022-11-17 21:18:42 -0300

    Added image modal for image-block

--- client/src/components/ImageBlock/index.tsx
+++ client/src/components/ImageBlock/index.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import { useQuery } from 'react-query'
 
 import { ContentReference, getImage, Image } from '../../firebase/functions'
+import { CustomModal } from '../'
 
 import './styles.scss'
 
@@ -9,6 +11,7 @@ interface ImageBlockProps {
 }
 
 export default function ImageBlock({ value }: ImageBlockProps) {
+	const [modalOpen, setModalOpen] = useState<boolean>(false)
 	const { data, isLoading, error } = useQuery<Image, Error>(`image-${value.id}`, () => getImage(value))
 
 	if (isLoading) return <></>
@@ -16,13 +19,28 @@ export default function ImageBlock({ value }: ImageBlockProps) {
 	if (!data) return <></>
 
 	return (
-		<figure className="d-flex justify-content-center">
-			<img
-				src={data?.image}
-				title={data?.title}
-				alt={data?.title}
-				className="img-fluid shadow-2-strong"
-			/>
-		</figure>
+		<>
+			{modalOpen ? (
+				<CustomModal close={() => setModalOpen(false)}>
+					<figure className="d-flex justify-content-center">
+						<img
+							src={data?.image}
+							title={data?.title}
+							alt={data?.title}
+							className="img-fluid shadow-2-strong"
+						/>
+					</figure>
+				</CustomModal>
+			) : <></>}
+			<figure onClick={() => setModalOpen(true)} className="image-block d-flex justify-content-center w-25">
+				<img
+					src={data?.image}
+					title={data?.title}
+					alt={data?.title}
+					className="img-fluid shadow-2-strong"
+				/>
+				<i className="fas fa-3x fa-search-plus"></i>
+			</figure>
+		</>
 	)
 }
